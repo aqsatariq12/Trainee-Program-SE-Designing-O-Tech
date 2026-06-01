3899c9c3264c1b13991ba3c7232806e71ec617b4
aqsatariq12 <aqsatariq2452003@gmail.com>
2026-06-02 00:50:48 +0500

    added testimonial section

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('.case-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $('.test-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        navText: [
+            '<i class="fa-solid fa-arrow-left"></i>',
+            '<i class="fa-solid fa-arrow-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1025: {
+                items: 2
+            }
+        }
+    });
+});

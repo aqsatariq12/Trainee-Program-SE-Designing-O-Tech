4a1d2fc5dd353ab1ece4abd8e2bdfb16948510d6
aqsatariq12 <aqsatariq2452003@gmail.com>
2026-06-02 16:52:51 +0500

    home section added

--- script.js
+++ script.js
@@ -37,4 +37,28 @@ $(document).ready(function () {
             }
         }
     });
+    $('.team-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        navText: [
+            '<i class="fa-solid fa-arrow-left"></i>',
+            '<i class="fa-solid fa-arrow-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            643:{
+                items:3
+            },
+            1025: {
+                items: 4
+            }
+        }
+    });
 });
